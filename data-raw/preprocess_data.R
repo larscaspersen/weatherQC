208a14e42cc4d1d7ac2c7cda5dee763633ef448a
larscaspersen <lars.caspersen@uni-bonn.de>
2022-07-08 18:57:09 +0200

    adjusted example data

--- data-raw/preprocess_data.R
+++ data-raw/preprocess_data.R
@@ -1,13 +1,53 @@
 library(chillR) #function to load temperature data
 
+# 
+# target_weather <- load_temperature_scenarios(path = "../applied_weatherQC_CA/prepared_weather_data/",
+#                                              prefix = "target-station")
+# 
+# aux_weather <- load_temperature_scenarios(path = "../applied_weatherQC_CA/prepared_weather_data/",
+#                                           prefix = "aux-station")
+# 
+# cimis_info <- read.csv("../applied_weatherQC_CA/cimis_info.csv")
+# 
+# target_info <- cimis_info[1,c("id", "Name", "Longitude", "Latitude")]
+# 
+# aux_info <- read.csv('../applied_weatherQC_CA/quality_control_aux-info.csv')
+# 
+# #subset for the weather stations close to the target_station
+# 
+# aux_info$dist <-  round(sp::spDistsN1(pts = as.matrix(aux_info[, c("Longitude", "Latitude")]),
+#                                       pt = c(target_info$Longitude, target_info$Latitude),
+#                                       longlat = TRUE), 2)
+# 
+# aux_info <- aux_info %>%
+#   filter(dist > 0 & dist <= 75) %>%
+#   select(-dist)
+# 
+# aux_weather <- aux_weather[aux_info$id]
+# 
+# save_temperature_scenarios(generated_temperatures = aux_weather,
+#                            path = 'data-raw/',
+#                            prefix = 'neighbour-weather')
+# 
+# write.csv(aux_info, file = 'data-raw/neighbour_info.csv', row.names = F)
+# 
+# 
+# 
+# write.csv(target_info, file = 'data-raw/weather_info.csv', row.names = F)
+# 
+# #write target weather station
+# write.csv(target_weather[[1]], file = 'data-raw/target-weather_cimis_2.csv',
+#           row.names = F)
+
+
 #load meta_data of target and neighbour stations
 target_info <- read.csv('data-raw/weather_info.csv')
 neighbour_info <- read.csv('data-raw/neighbour_info.csv')
 
 #load weather observation of target and neighbour stations
-target_weather <- read.csv('data-raw/target_weather_cimis_7.csv')
+target_weather <- read.csv('data-raw/target-weather_cimis_2.csv')
 neighbour_weather <- load_temperature_scenarios(path = 'data-raw/',
-                                             prefix = 'neighbour_weather')
+                                             prefix = 'neighbour-weather')
 
 #download cimis stations and ucipm stations
 #save as objects in the end
